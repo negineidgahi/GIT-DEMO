48677921c0ffa0ad80a5a7620cd60f182b861c53
negin <negin.eidgahi68@thehutgroup.com>
2022-09-27 13:54:48 +0100

    jupiter

--- hello.py
+++ hello.py
@@ -1,5 +1,18 @@
-def hello():
+import sys
+
+
+def helloworld():
 	print('Helloworld!')
+
+def hellojupiter():
+	print('Hellojupiter')
+
+def hello():
+	if sys.argv[1] == "Jupiter":
+		hellojupiter()
+	else:
+		helloworld()
+
 if __name__ == '__main__':
 	hello()
 
