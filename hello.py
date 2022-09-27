e21c96c995fe559a412936f766639ba1b71315f5
negin <negin.eidgahi68@thehutgroup.com>
2022-09-27 13:43:45 +0100

    mars

--- hello.py
+++ hello.py
@@ -1,5 +1,16 @@
+import sys
+
 def hello():
-	print('Helloworld!')
+	if sys.argv[1] == "Mars":
+		hellomars()
+	else:
+		helloworld()
+
+def hellomars():
+	print('Hello mars!')
+
+def helloworld():
+	print("Hello world!")
 if __name__ == '__main__':
 	hello()
 
